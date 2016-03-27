3afa9ac2907e2c80471b2a806250798e687baeb4
Salmon <grass.house@hotmail.com>
2016-03-28 03:17:42 +0800

    HW01 all problems are completed.

--- lab1/counting.cu
+++ lab1/counting.cu
@@ -122,22 +122,66 @@ void CountPosition(const char *text, int *pos, int text_size) {
 }
 
 // Problem 2: Find the Heads.
+template<int INT>
+class equal {
+public:
+    __device__ bool operator () (int x) { return x == INT; }
+};
+
 int ExtractHead(const int *pos, int *head, int text_size) {
-    int *buffer;
     int nhead;
-    cudaMalloc(&buffer, sizeof(int)*text_size*2); // this is enough
     thrust::device_ptr<const int> pos_d(pos);
-    thrust::device_ptr<int> head_d(head), flag_d(buffer), cumsum_d(buffer+text_size);
-
-    // TODO
-    nhead = 1;
-
-
-    cudaFree(buffer);
+    thrust::device_ptr<int> head_d(head);
+    // copy_if.
+    auto head_end = thrust::copy_if(
+        thrust::counting_iterator<int>(0),
+        thrust::counting_iterator<int>(text_size),
+        pos_d,
+        head_d,
+        equal<1>()
+    );
+    nhead = head_end - head_d;
     return nhead;
 }
 
 // Problem 3: Be Creative!
+__global__ void transformToSwapPairs(char *input, int fsize) {
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    // Current char.
+    char cur_c = input[idx];
+    int idx_in_str = 0;
+    // 
+    if ((cur_c >= 'A' && cur_c <= 'Z') || (cur_c >= 'a' && cur_c <= 'z')) {
+        // Get the index in the current string.
+        for (int i = idx - 1; i >= 0; i--) {
+            char c = input[i];
+            if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z')) {
+                idx_in_str++;
+            } else {
+                break;
+            }
+        }
+        // Swap by string pairs.
+        if (! (idx_in_str % 2) && idx < fsize) {
+            char c = input[idx + 1];
+            if (c >= 'A' && c <= 'Z') {
+                if (cur_c >= 'a' && cur_c <= 'z')
+                    input[idx] = c + 32, input[idx + 1] = cur_c - 32;
+                else
+                    input[idx] = c, input[idx + 1] = cur_c;
+            } else if (c >= 'a' && c <= 'z') {
+                if (cur_c >= 'a' && cur_c <= 'z')
+                    input[idx] = c, input[idx + 1] = cur_c;
+                else
+                    input[idx] = c - 32, input[idx + 1] = cur_c + 32;
+            }
+        }
+    }
+}
+
 void Part3(char *text, int *pos, int *head, int text_size, int n_head) {
+    int threads = 32;
+    int blocks = ceil((float) text_size / threads);;
+    transformToSwapPairs <<< blocks, threads >>> (text, text_size);
     return;
 }
